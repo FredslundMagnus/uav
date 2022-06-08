8774ac1656628a43eeae6e70fc4572c4386d5287
FredslundMagnus <s183905@student.dtu.dk>
2022-06-08 09:07:02 +0200

    fix

---
+++ Exercises/PART 6/6_4.m
@@ -0,0 +1,8 @@
+order = 11;
+corridors.times = [1.5 2 2.5 3.25];
+corridors.x_lower = [0.5 8.65 8.65 8.65];
+corridors.x_upper = [8.5 9.35 9.35 9.35];
+corridors.y_lower = [-0.35 -0.35 -0.35 0.5];
+corridors.y_upper = [0.25 0.25 0.25 8.5];
+corridors.z_lower = [0.5 0.5 0.5 0.5];
+corridors.z_upper = [1.5 1.5 1.5 1.5];
