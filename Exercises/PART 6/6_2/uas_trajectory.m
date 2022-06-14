a277ee1967491fbd11e4c9749770ad2cd0dc003b
s183914 <s183914@student.dtu.dk>
2022-06-14 15:09:08 +0200

    e

--- Exercises/PART 6/6_2/uas_trajectory.m
+++ Exercises/PART 6/6_2/uas_trajectory.m
@@ -56,3 +56,40 @@ corridors.z_upper = [1.5 1.5 1.5 1.5];
 make_plots = true;
 
 poly_traj = uas_minimum_snap(knots, order, waypoints, corridors, make_plots);
+
+route = [0 0 1 ; 9 0 1 ; 9 9 1]; % Original: 6.4
+
+% route = [0 0 1; 1 0 1; 2 0 1; 3 0 1; 4 0 1; 5 0 1; 6 0 1; 7 0 1; 8 0 1; 9 0 1; 9 1 1; 9 2 1; 9 3 1; 9 4 1; 9 5 1; 8 5 1; 7 5 1; 7 6 1; 7 7 1; 6 7 1; 5 7 1; 5 6 1; 5 5 1; 4 5 1; 3 5 1];
+wall_color = [0.8 0.2 0.2];
+sample_time = 4e-2;
+publish_rate = 1 * sample_time;
+x0 = 36;
+y0 = 80;
+z0 = 1;
+g = 9.80665 ;
+mass_drone = 0.68 ;
+mass_rod = 0.0;
+mass_tip = 0;
+mass_total = mass_drone + mass_rod + mass_tip;
+stiffness_rod = 100 ;
+critical_damping_rod = 2 * sqrt(mass_total * stiffness_rod) ;
+stiffness_wall = 100 ;
+critical_damping_wall = 2 * sqrt(mass_total * stiffness_wall) ;
+inertia_xx = 0.007 ;
+inertia_yy = 0.007 ;
+inertia_zz = 0.012 ;
+arm_length = 0.17 ;
+rotor_offset_top = 0.01 ;
+motor_constant = 8.54858e-06 ;
+moment_constant = 0.016 ;
+max_rot_velocity = 838 ;
+allocation_matrix = ...
+    [1 1 1 1
+     0 arm_length 0 -arm_length
+     -arm_length 0 arm_length 0
+     -moment_constant moment_constant -moment_constant moment_constant] ;
+mix_matrix = inv(motor_constant * allocation_matrix) ;
+air_density = 1.2041;
+drag_coefficient = 0.47;
+reference_area = pi * 75e-3^2;
+
